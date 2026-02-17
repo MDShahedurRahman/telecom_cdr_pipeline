c95c5f50806846b7922032e63e0013017eaa2ee1
Md Shahedur Rahman <mr6139@nyu.edu>
2026-02-16 23:42:06 -0500

    Add config paths

---
+++ config.py
@@ -0,0 +1,7 @@
+RAW_FILE = "data/call_records.csv"
+
+BRONZE_PATH = "output/bronze/"
+SILVER_PATH = "output/silver/"
+ANOMALY_PATH = "output/silver/anomaly_flagged/"
+GOLD_PATH = "output/gold/"
+REPORT_PATH = "output/reports/"
