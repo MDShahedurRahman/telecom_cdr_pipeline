f3d32c7826a891c80f94ef6db14591493e7e54ed
Md Shahedur Rahman <mr6139@nyu.edu>
2026-02-17 00:13:38 -0500

    Build dim_call_type

--- jobs/gold_star_schema.py
+++ jobs/gold_star_schema.py
@@ -4,4 +4,8 @@ def build_star_schema(df, gold_path):
         "customer_id", "customer_name", "city", "country"
     ).distinct()
 
+    dim_call_type = df.select(
+        "call_type"
+    ).distinct()
+
     return fact_calls
