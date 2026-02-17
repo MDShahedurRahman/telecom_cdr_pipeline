7f0ae88a5b1a674ea64c1a6e30d478d77f451c11
Md Shahedur Rahman <mr6139@nyu.edu>
2026-02-16 23:45:39 -0500

    Add raw call dataset

--- utils/schema_definitions.py
+++ utils/schema_definitions.py
@@ -3,3 +3,14 @@ from pyspark.sql.types import StringType, IntegerType, DoubleType
 
 
 def cdr_schema():
+    return StructType([
+        StructField("call_id", IntegerType(), True),
+        StructField("customer_id", StringType(), True),
+        StructField("customer_name", StringType(), True),
+        StructField("call_type", StringType(), True),
+        StructField("duration_minutes", DoubleType(), True),
+        StructField("call_cost", DoubleType(), True),
+        StructField("call_date", StringType(), True),
+        StructField("city", StringType(), True),
+        StructField("country", StringType(), True)
+    ])
