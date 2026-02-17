4467d4251983af366fdb3eb8631cea465ce36f28
Md Shahedur Rahman <mr6139@nyu.edu>
2026-02-17 00:18:10 -0500

    Add top customer spending query

--- jobs/business_queries.py
+++ jobs/business_queries.py
@@ -1,1 +1,7 @@
 from pyspark.sql.functions import sum, count, desc
+
+
+def top_customers_by_call_cost(df):
+    return df.groupBy("customer_name") \
+        .agg(sum("call_cost").alias("total_spent")) \
+        .orderBy(desc("total_spent"))
