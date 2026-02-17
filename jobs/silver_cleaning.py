747b386572279176795778d9bdfa4b46cc04292c
Md Shahedur Rahman <mr6139@nyu.edu>
2026-02-16 23:53:59 -0500

    Convert call_date format

--- jobs/silver_cleaning.py
+++ jobs/silver_cleaning.py
@@ -4,6 +4,7 @@ from pyspark.sql.functions import col, to_date
 def clean_call_data(df, silver_path):
 
     cleaned_df = df.dropDuplicates() \
-        .dropna()
+        .dropna() \
+        .withColumn("call_date", to_date(col("call_date")))
 
     return cleaned_df
