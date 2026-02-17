f7ba81e02aa4ad416bac8d7f142409a9cb4946fb
Md Shahedur Rahman <mr6139@nyu.edu>
2026-02-16 23:49:03 -0500

    Implement ingest_call_records()

--- jobs/bronze_ingestion.py
+++ jobs/bronze_ingestion.py
@@ -1,1 +1,12 @@
 from utils.schema_definitions import cdr_schema
+
+
+def ingest_call_records(spark, input_file, bronze_path):
+
+    df = spark.read.csv(
+        input_file,
+        header=True,
+        schema=cdr_schema()
+    )
+
+    return df
