2522ad9aecb64773fbb9a44e8a93fc15096323c9
Md Shahedur Rahman <mr6139@nyu.edu>
2026-02-17 00:08:38 -0500

    Add long call rule

--- jobs/anomaly_detection.py
+++ jobs/anomaly_detection.py
@@ -3,4 +3,8 @@ from pyspark.sql.functions import col, when
 
 def detect_anomalies(df, anomaly_path):
 
+    anomaly_df = df.withColumn(
+        when(col("duration_minutes") > 120, "LONG_CALL")
+    )
+
     return anomaly_df
