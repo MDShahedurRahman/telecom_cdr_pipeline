6956c03a9f28ce01c6138f0c60187594df040614
Md Shahedur Rahman <mr6139@nyu.edu>
2026-02-17 00:20:33 -0500

    Implement main() method

---
+++ main.py
@@ -0,0 +1,20 @@
+from config import RAW_FILE, BRONZE_PATH, SILVER_PATH, ANOMALY_PATH, GOLD_PATH
+from utils.spark_session import get_spark_session
+
+from jobs.bronze_ingestion import ingest_call_records
+from jobs.silver_cleaning import clean_call_data
+from jobs.anomaly_detection import detect_anomalies
+from jobs.gold_star_schema import build_star_schema
+
+from jobs.business_queries import (
+    top_customers_by_call_cost,
+    revenue_by_call_type,
+    anomaly_breakdown
+)
+
+
+def main():
+
+
+if __name__ == "__main__":
+    main()
