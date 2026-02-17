bb1c9041eb8dc00bf0a9e716916e0eac3f4105a1
Md Shahedur Rahman <mr6139@nyu.edu>
2026-02-17 00:18:42 -0500

    Add revenue by call type query

--- jobs/business_queries.py
+++ jobs/business_queries.py
@@ -5,3 +5,9 @@ def top_customers_by_call_cost(df):
     return df.groupBy("customer_name") \
         .agg(sum("call_cost").alias("total_spent")) \
         .orderBy(desc("total_spent"))
+
+
+def revenue_by_call_type(df):
+    return df.groupBy("call_type") \
+        .agg(sum("call_cost").alias("total_revenue")) \
+        .orderBy(desc("total_revenue"))
