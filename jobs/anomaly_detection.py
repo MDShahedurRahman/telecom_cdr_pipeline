f452e72516719589cd72c97a06ecca0ab6cd7efb
Md Shahedur Rahman <mr6139@nyu.edu>
2026-02-17 00:09:52 -0500

    Add anomaly_flag column

--- jobs/anomaly_detection.py
+++ jobs/anomaly_detection.py
@@ -4,6 +4,7 @@ from pyspark.sql.functions import col, when
 def detect_anomalies(df, anomaly_path):
 
     anomaly_df = df.withColumn(
+        "anomaly_flag",
         when(col("duration_minutes") > 120, "LONG_CALL")
         .when(col("call_type") == "International", "INTERNATIONAL_CALL")
     )
