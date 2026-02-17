f6b933d6c8c66face1358f9e31941c63cca40762
Md Shahedur Rahman <mr6139@nyu.edu>
2026-02-16 23:53:21 -0500

    Add null handling

--- jobs/silver_cleaning.py
+++ jobs/silver_cleaning.py
@@ -3,6 +3,7 @@ from pyspark.sql.functions import col, to_date
 
 def clean_call_data(df, silver_path):
 
-    cleaned_df = df.dropDuplicates()
+    cleaned_df = df.dropDuplicates() \
+        .dropna()
 
     return cleaned_df
